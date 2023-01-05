cf3f32c39d5479ef196ad9e373e41985cd3725ca
Dane Osborne <u7282852@anu.edu.au>
2023-01-05 14:41:59 +1100

    comitting to rever

--- src/main/gui/GameController.java
+++ src/main/gui/GameController.java
@@ -9,6 +9,9 @@ import javafx.scene.control.Label;
 import java.awt.*;
 
 public class GameController {
+    public GameController(TicTacToe game) {
+        this.game = game;
+    }
 
     TicTacToe game;
 
@@ -75,10 +78,8 @@ public class GameController {
     private Button b9;
     public void handleButtonClick(){
         if(game.isMoveValid(curMove)){
+            curLabel.setOpacity(1);
             game.makeMove(curMove);
-        }
-        else{
-
         }
     }
 
@@ -88,29 +89,47 @@ public class GameController {
 
     public void handleb1(ActionEvent e){
         curMove = 0;
+        curLabel = game.getTurn() == 1 ? xl1 : ol1;
+        handleButtonClick();
     }
     public void handleb2(ActionEvent e){
         curMove = 1;
+        curLabel = game.getTurn() == 1 ? xl2 : ol2;
+        handleButtonClick();
     }
     public void handleb3(ActionEvent e){
         curMove = 2;
+        curLabel = game.getTurn() == 1 ? xl3 : ol3;
+        handleButtonClick();
     }
     public void handleb4(ActionEvent e){
         curMove = 3;
+        curLabel = game.getTurn() == 1 ? xl4 : ol4;
+        handleButtonClick();
     }
     public void handleb5(ActionEvent e){
         curMove = 4;
+        curLabel = game.getTurn() == 1 ? xl5 : ol5;
+        handleButtonClick();
     }
     public void handleb6(ActionEvent e){
         curMove = 5;
+        curLabel = game.getTurn() == 1 ? xl6 : ol6;
+        handleButtonClick();
     }
     public void handleb7(ActionEvent e){
         curMove = 6;
+        curLabel = game.getTurn() == 1 ? xl7 : ol7;
+        handleButtonClick();
     }
     public void handleb8(ActionEvent e){
         curMove = 7;
+        curLabel = game.getTurn() == 1 ? xl8 : ol8;
+        handleButtonClick();
     }
     public void handleb9(ActionEvent e) {
         curMove = 8;
+        curLabel = game.getTurn() == 1 ? xl9 : ol9;
+        handleButtonClick();
     }
 }
