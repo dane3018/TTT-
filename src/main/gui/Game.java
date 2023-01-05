29b443724597b936304abd8da2d0db61346e9c80
Dane Osborne <u7282852@anu.edu.au>
2023-01-05 14:45:26 +1100

    Revert "comitting to rever"
    This reverts commit cf3f32c39d5479ef196ad9e373e41985cd3725ca.

--- src/main/gui/Game.java
+++ src/main/gui/Game.java
@@ -4,27 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import main.TicTacToe;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Game extends Application {
-    TicTacToe game = new TicTacToe();
-    GameController gameController;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("main.gui.Game.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("Game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 700);
-        gameController = fxmlLoader.getController();
-        gameController.setGame(game);
         primaryStage.setTitle("TicTacToe");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
